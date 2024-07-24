45a3ca688cd7c868da200ec4f3d1dce72902d684
JUN <gurwns232@naver.com>
2024-07-24 19:37:57 +0900

    feat: setup react query & get user data

---
+++ app/hook/useUser.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { supabaseClient } from '@/lib/supabase/client'
+import { useQuery } from '@tanstack/react-query'
+
+const initUser = {
+  created_at: '',
+  display_name: '',
+  email: '',
+  id: '',
+  image_url: '',
+}
+
+const useUser = () => {
+  return useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const supabase = await supabaseClient()
+      const { data } = await supabase.auth.getSession()
+
+      if (data.session?.user) {
+        // fetch user information profile
+        const { data: user } = await supabase
+          .from('profiles')
+          .select('*')
+          .eq('id', data.session.user.id)
+          .single()
+
+        return user
+      }
+      return initUser
+    },
+  })
+}
+
+export default useUser
